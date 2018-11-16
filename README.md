b0f6f2a3b81036a12d2812499d3cbd203c172a84
Perry Choy <perry1629@gmail.com>
2018-11-16 14:25:32 -0500

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# sportsbox
+# project1
+
+sPORTS bOX serves the sports lover; it is a place to view highlights, scores, and schedules of your favourite teams and leagues
+
+Current Leagues on sPORTSbOX:
+NFL, NHL, NBA, MLB
+
+Possible Future Leagues:
+MLS, PGA, NCAA, WNBA, WHL
+
+How sPORTSbOX works:
+
+On the homepage, you'll see general sports highlights /news stories. On the navbar, select the sports league of your choice (NFL, NHL, NBA, or MLB) and then: 
+
+Select one of three tabs:
+ - Game Schedules
+ - Scoreboards
+ - News
+
+ OR
+
+ Search for a team (i.e. go to MLB & type "Toronto Blue Jays" into search box)
+
+System will prompt "You have to enter a team name for searching", if there's nothing entered in the input field to search, and system will return "(team name): There is such team found!".
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,253 @@
+$(function () {
+  var leg = {
+    nhl: "2a2875c0-ee7b-48e4-b9b7-cd99f2",
+    nfl: "b569ec72-8849-44ac-9f3d-6f515d",
+    mlb: "733b2e56-34ff-4972-88db-8e49b0",
+    nba: "7bab1d20-fabe-4954-ac75-b73bae"
+  };
+
+  var count = 0;
+  var news = $("#news");
+  var time = "20181104";
+  // moment().subtract(1, "days").format("YYYYMMDD");
+  function imageScreen(choice, where) {
+    var j;
+
+    var sportURL =
+      "https://webhose.io/filterWebContent?token=38a8d800-55c4-4e21-8dff-0b16f52137c1&format=json&ts=1540846661784&sort=crawled&q=text%3A" +
+      choice +
+      "%20thread.country%3AUS%20site_category%3Asports%20site_type%3Anews";;
+    fetch(sportURL)
+      .then(resp => resp.json())
+      .then(function (response) {
+        for (var rowAmount = 0; rowAmount < 2; rowAmount++) {
+          j = 3;
+          for (var colAmount = 0; colAmount < j; colAmount++) {
+            if (response.posts[count].thread.main_image != "") {
+              if (response.posts[count].language == "english") {
+                if (response.posts[count].thread.section_title != "") {
+                  var news = $(
+                    '<div class="news-post-holder col-4"><img class="img-responsive" src="' +
+                    response.posts[count].thread.main_image +
+                    '" alt=""><div class="news-post-detail"><span class="date">' +
+                    moment(response.posts[count].thread.published).format(
+                      "MMM Do YYYY"
+                    ) +
+                    '</span><h2><a href="' +
+                    response.posts[count].thread.url +
+                    '">' +
+                    response.posts[count].thread.section_title +
+                    "</a></h2><p>" +
+                    response.posts[count].thread.title +
+                    "</p></div></div>"
+                  );
+
+                  $(where).append(news);
+                } else {
+                  j++;
+                }
+              } else {
+                j++;
+              }
+            } else {
+              j++;
+            }
+            count++;
+          }
+        }
+        // }
+      });
+  }
+
+  setTimeout(imageScreen("nhl", news), 1001);
+  setTimeout(imageScreen("nfl", news), 1001);
+  // setTimeout(imageScreen("mlb"), 3001);
+  setTimeout(imageScreen("nba", news), 1001);
+  // scoreboard=============================================================
+  function scoreScreen(api, league) {
+    j = 4;
+    var password = "tanakan13";
+    var sportURL =
+      "https://api.mysportsfeeds.com/v1.0/pull/" +
+      league +
+      "/2018-regular/scoreboard.json?fordate=" +
+      time +
+      "";
+
+    $.ajax({
+      url: sportURL,
+      method: "GET",
+      dataType: "json",
+      async: false,
+      headers: {
+        Authorization: "Basic " + btoa(api + ":" + password)
+      }
+    }).then(function (response) {
+      for (var i = 0; i < j; i++) {
+        var score =
+         "<div class='col-2' id='block'><div class='d-flex'><div id='date'>" +
+         moment(response.scoreboard.gameScore[i].game.date).format("MMM Do") +
+         "</div></div><div class='d-flex'>AWAY:&nbsp&nbsp<div>" +
+         response.scoreboard.gameScore[i].game.awayTeam.Abbreviation +
+         "</div><div>&nbsp" +
+         response.scoreboard.gameScore[i].awayScore +
+         "</div></div><div class='d-flex'>HOME:&nbsp&nbsp<div>" +
+         response.scoreboard.gameScore[i].game.homeTeam.Abbreviation +
+         "&nbsp</div><div>&nbsp&nbsp" +
+         response.scoreboard.gameScore[i].homeScore +
+         "</div></div></div>";
+
+        $("#score").append(score);
+      }
+    });
+    // ++++++++==========================================
+  }
+
+  // Create matchup content
+  function createMatchupContent(results, searchKey) {
+    $("#matchup-page").empty(); // Clear the page first before showing new data
+    if (results == "") {
+      $("#matchup-page").html(searchKey + ": There is no such team found!");
+    }
+
+    var gameDate = "";
+    for (var i = 0; i < results.length; i++) {
+      // Game date conversion
+      var convertedDate = moment(results[i].datetime_local.substr(0, 10), 'YYYY-MM-DD');
+      // Table header construction for different game dates
+      if (convertedDate.toString() != gameDate.toString()) {
+        gameDate = convertedDate;
+        var dTable = $("<table class='day-table'>");
+        var sectionHeader = $(`<div class='section-header'>${gameDate.format("dddd, MMM D")}</div>`);
+        var tHead = $("<thead>");
+        var trRow = $("<tr>");
+        var thMatchupCol = $("<th class='matchup-col-header'>Matchup</th>");
+        var thTimeCol = $("<th class='time-col-header'>Time</th>");
+        var thTicketsCol = $("<th class='tickets-col-header'>Tickets</th>");
+        var tBody = $("<tbody>");
+
+        trRow.append(thMatchupCol, thTimeCol, thTicketsCol);
+        tHead.append(trRow);
+        dTable.append(tHead);
+        $("#matchup-page").append(sectionHeader, dTable);
+      }
+
+      var tRow = $("<tr class='league-schedule-row'>");
+      var tD1 = $("<td class='matchup-teams'>");
+      var spanTeamAway = $("<span class='matchup-team-away'>").text(results[i].performers[1].name);
+      var spanAtSign = $("<span class='at-sign'>").text(" @ ");
+      var spanTeamHome = $("<span class='matchup-team-home'>").text(results[i].performers[0].name);
+      tD1.append(spanTeamAway, spanAtSign, spanTeamHome);
+      var convertedTime = moment(results[i].datetime_local.substr(11, 8), 'HH:mm:ss');
+      var gameTime = convertedTime.format("LT");
+      var tD2 = $(`<td><div class='matchup-time'>${gameTime} ET</div></td>`);
+      var tD3 = $(`<td><a class="matchup-tickets" target='_blank' href=${results[i].url}>Buy Tickets</a></td>`);
+
+      tRow.append(tD1, tD2, tD3);
+      tBody.append(tRow);
+      dTable.append(tBody);
+    }
+  }
+
+  // Display league game schedule function - limited to 10 games
+  function displayLeagueMatchupInfo(searchKey) {
+    var searchKey = searchKey.replace(/\s/g, '-');
+    var queryURL = "https://api.seatgeek.com/2/events?taxonomies.name=" + searchKey + "&client_id=MTM4MjYzODl8MTU0MTUyOTc1NS42Mg";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: function (err) {
+          console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
+        }
+      })
+      .done(function (response) {
+        var results = response.events;
+
+        createMatchupContent(results, searchKey);
+      });
+  }
+
+  // Display team game schedule function - limited to 10 games
+  function displayTeamMatchupInfo(searchKey) {
+    var searchKey = searchKey.replace(/\s/g, '-');
+    var queryURL = "https://api.seatgeek.com/2/events?performers.slug=" + searchKey + "&client_id=MTM4MjYzODl8MTU0MTUyOTc1NS42Mg";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: function (err) {
+          console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
+        }
+      })
+      .done(function (response) {
+        var results = response.events;
+
+        createMatchupContent(results, searchKey);
+      });
+  }
+
+  // League search
+  $(".main-btn").on("click", "[data-league='league']", function () {
+    var league = $(this).attr("id");
+    var where = $("#leagueNews");
+    $("#news").addClass("hide");
+    $("[data-tab='tabSchedule']").removeClass("hide");
+    $("[data-tab='tabScore']").removeClass("hide");
+    $("[data-tab='tabNews']").removeClass("hide");
+    $("#searchedPages").removeClass("hide");
+    $("#score").addClass("hide");
+    $("#leagueNews").empty();
+    $("#leagueNews").addClass("hide");
+
+    $("#score").empty();
+    displayLeagueMatchupInfo(league);
+     //Oleksii MLB scoreboard fix
+     if (league == "mlb") {
+      scoreScreen(leg.nhl, "nhl");
+    } else {
+      scoreScreen(leg[league], league);
+    }
+    // End MLB scoreboard fix
+    setTimeout(imageScreen(league, where), 2000);
+  });
+
+  $(document).on("click", "#home", function (e) {
+    e.preventDefault();
+    $("#news").removeClass("hide");
+    $("[data-tab='tabSchedule']").addClass("hide");
+    $("[data-tab='tabScore']").addClass("hide");
+    $("#searchedPages").addClass("hide");
+    $("#score").empty();
+    $("#leagueNews").empty();
+  });
+
+  $(document).on("click", '[data-tab="tabScore"]', function (e) {
+    e.preventDefault();
+    $("#searchedPages").addClass("hide");
+    $("#score").removeClass("hide");
+    $("#leagueNews").addClass("hide");
+  });
+  $(document).on("click", '[data-tab="tabSchedule"]', function (e) {
+    e.preventDefault();
+    $("#score").addClass("hide");
+    $("#searchedPages").removeClass("hide");
+    $("#leagueNews").addClass("hide");
+  });
+  $(document).on("click", '[data-tab="tabNews"]', function (e) {
+    e.preventDefault();
+    $("#score").addClass("hide");
+    $("#searchedPages").addClass("hide");
+    $("#leagueNews").removeClass("hide");
+  });
+
+  // Team search by clicking submit
+  $('#search-submit').on('click', function () {
+    var teamName = $('#search-input').val().trim();
+    if (teamName != "") {
+      displayTeamMatchupInfo(teamName);
+    } else
+      alert("You have to enter a team name for searching!");
+    $('#search-input').val('');
+  });
+});
